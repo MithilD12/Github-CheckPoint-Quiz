ec634daeb7c5a3ec8a9f05a6c50778fbeb7ac656
MithilD12 <45392148+MithilD12@users.noreply.github.com>
2018-12-03 07:45:38 -0500

    Created test variable
    Created variable and assigned it a value

---
+++ GitHub Test.java
@@ -0,0 +1,12 @@
+public class GitHub Test{
+
+	private String test;
+
+
+	public GitHub Test(){
+	test  = "This is my GitHub test";
+	}
+	public String toString(){
+		return "\"+test+\"";
+	}
+}
---
+++ GitTest.java
@@ -0,0 +1,10 @@
+public class GitTest{
+
+	private String test;
+
+
+	public GitTest(){
+	test  = "This is my GitHub test";
+	}
+
+}
